003d9d0cf9162519aa02b9fa584fc4690cfda4b7
rublon <rublon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-12 13:53:31 +0000

    Tag for version 1.3.0.
    git-svn-id: https://plugins.svn.wordpress.org/rublon/tags/1.3.0@988284 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/rublon2factor_initialization.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Code to be run on Rublon2Factor plug-in initialization
+ *
+ * @package   rublon2factor\includes
+ * @author     Rublon Developers http://www.rublon.com
+ * @copyright  Rublon Developers http://www.rublon.com
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License, version 2 
+ */
+
+/**
+ * Initialize
+ */
+function rublon2factor_plugins_loaded() {
+
+	// Initialize Rublon2Factor plug-in helper
+	RublonHelper::init();
+
+	// check for Rublon-type actions
+	RublonHelper::checkForActions();
+
+}
