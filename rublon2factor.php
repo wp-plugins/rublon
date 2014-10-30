069ec7bdde433e5bb9366599d5125171a6967080
rublon <rublon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 13:25:18 +0000

    Tag for v. 2.1.1.
    git-svn-id: https://plugins.svn.wordpress.org/rublon/tags/2.1.1@1016683 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ rublon2factor.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: Rublon
+Text Domain: rublon
+Plugin URI: http://wordpress.org/plugins/rublon/
+Description: Rublon provides stronger security for online accounts through invisible two-factor authentication. It protects your accounts from sign-ins from unknown devices, even if your passwords get stolen.
+Version: 2.1.1
+Author: Rublon
+Author URI: https://rublon.com
+License: http://opensource.org/licenses/gpl-license.php GNU Public License, version 2 
+*/
+
+
+/*
+ * Define some constants for future usage
+*/
+define('RUBLON2FACTOR_PLUGIN_URL', plugins_url() . '/' . basename(dirname(__FILE__)));
+define('RUBLON2FACTOR_BASE_PATH', dirname(plugin_basename(__FILE__)));
+define('RUBLON2FACTOR_PLUGIN_PATH', __FILE__);
+
+/**
+ * Ensure proper version migration
+**/
+
+function rublon2factor_add_settings_link($links, $file) {
+
+	static $rublon2factor_plugin = null;
+
+	if (is_null($rublon2factor_plugin)) {
+		$rublon2factor_plugin = plugin_basename(__FILE__);
+	}
+
+	if ($file == $rublon2factor_plugin) {
+		$settings_link = '<a href="admin.php?page=rublon">' . __('Settings', 'rublon') . '</a>';
+		array_unshift($links, $settings_link);
+	}
+	return $links;
+
+}
+
+add_filter('plugin_action_links', 'rublon2factor_add_settings_link', 10, 2);
+
+require_once dirname(__FILE__) . '/includes/libs/RublonImplemented/Rublon2FactorWordPress.php';
+require_once dirname(__FILE__) . '/includes/libs/RublonImplemented/Rublon2FactorGUIWordPress.php';
+
+/*
+ * Include plug-in files
+*/
+require_once dirname(__FILE__) . '/includes/rublon2factor_helper.php';
+require_once dirname(__FILE__) . '/includes/rublon2factor_multisite_helper.php';
+require_once dirname(__FILE__) . '/includes/rublon2factor_cookies.php';
+require_once dirname(__FILE__) . '/includes/rublon2factor_requests.php';
+require_once dirname(__FILE__) . '/includes/rublon2factor_initialization.php';
+require_once dirname(__FILE__) . '/includes/rublon2factor_admin.php';
+require_once dirname(__FILE__) . '/includes/rublon2factor_hooks.php';
+
+/*
+ * Include Rublon PHP libraries
+*/
+require_once dirname(__FILE__) . '/includes/libs/RublonConsumerRegistration/RublonConsumerRegistrationTemplate.php';
+require_once dirname(__FILE__) . '/includes/libs/RublonImplemented/RublonConsumerRegistrationWordPress.php';
+
+// Initialize rublon2factor plug-in
+add_action('plugins_loaded', 'rublon2factor_plugins_loaded', 9);
---
+++ uninstall.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Code to be run during plug-in uninstall process
+ *
+ * @package   rublon2factor
+ * @author     Rublon Developers http://www.rublon.com
+ * @copyright  Rublon Developers http://www.rublon.com
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License, version 2  
+ */
+
+if (!current_user_can('activate_plugins')) {
+	exit;
+}
+
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+	exit;
+}
+
+require_once dirname(__FILE__) . '/includes/rublon2factor_helper.php';
+require_once dirname(__FILE__) . '/includes/rublon2factor_multisite_helper.php';
+
+/**
+ * Perform database and options clean-up before deleting plug-in
+ */
+function rublon2factor_plugin_uninstall() {
+
+	// Clear settings for all sites
+	if (RublonMultisiteHelper::isMultisite()) {
+		RublonMultisiteHelper::uninstallMultisite();
+	} else {
+		delete_option(RublonHelper::RUBLON_SETTINGS_KEY);
+		delete_option(RublonHelper::RUBLON_ADDITIONAL_SETTINGS_KEY);
+		delete_option(RublonHelper::RUBLON_REGISTRATION_SETTINGS_KEY);
+		
+		$all_user_ids = get_users(array(
+			'fields' => 'id',
+		));
+		foreach ($all_user_ids as $user_id) {
+			delete_user_meta($user_id, RublonHelper::RUBLON_META_PROFILE_ID);
+			delete_user_meta($user_id, RublonHelper::RUBLON_META_USER_PROTTYPE);
+			delete_user_meta($user_id, RublonHelper::RUBLON_META_AUTH_CHANGED_MSG);
+		}		
+	}
+
+}
+
+// Call the clean-up function
+rublon2factor_plugin_uninstall();
