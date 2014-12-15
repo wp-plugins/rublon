c96bacfdabf4b5011239ba86efe5f13c4fe2e8ab
rublon <rublon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-15 08:31:34 +0000

    2.1.6 tagged.
    git-svn-id: https://plugins.svn.wordpress.org/rublon/tags/2.1.6@1044776 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/libs/RublonImplemented/Rublon2FactorGUIWordPress.php
@@ -0,0 +1,161 @@
+<?php
+
+require_once dirname (__FILE__) . '/../Rublon/Rublon2FactorGUI.php';
+
+class Rublon2FactorGUIWordPress extends Rublon2FactorGUI {
+	
+	public static $instance;
+	
+	public static function getInstance() {
+		if (empty(self::$instance)) {
+			$current_user = wp_get_current_user();
+			self::$instance = new self(
+				RublonHelper::getRublon(),
+				RublonHelper::getUserId($current_user),
+				RublonHelper::getUserEmail($current_user),
+				$logoutListener = true
+			);
+			// Embed consumer script
+			add_action('admin_footer', array(self::$instance, 'renderConsumerScript'));
+			add_action('wp_footer', array(self::$instance, 'renderConsumerScript'));
+		}
+		return self::$instance;
+	}
+	
+	
+
+
+	public function getActivationURL() {
+
+		return site_url('?rublon=init-registration&rublon_nonce='  . RublonHelper::getNonce());
+
+	}
+
+
+	/**
+	 * Return the activation button without the user box
+	 *
+	 * @return string
+	 */
+	public function getRegistrationButton() {
+	
+		$result = (string)$this->getConsumerScript();
+		$button = (string)$this->createActivationButton(null)->setLabel(__('Enable protection', 'rublon'));
+		$result .= '<div class=data-can-activate="' . $this->canUserActivate() . '">';
+		$result .= $button;
+		$result .= '</div>';
+		return $result;
+	
+	}
+
+
+	/**
+	 * Returns Rublon Button for plugin's registration.
+	 * 
+	 * Since the registration is now handled automatically,
+	 * the button is not necessary.
+	 *
+	 * @return RublonButton
+	 */
+	protected function createActivationButton($activationURL) {
+		return '';
+	}
+
+
+	/**
+	 * Create Trusted Devices Widget container for WP Dashboard 
+	 * 
+	 * @return string
+	 */
+	public function getTDMWidget($withConsumerScript = true) {
+
+		$result = '';
+
+		$current_user = wp_get_current_user();
+		$protection_type = RublonHelper::YES;
+		RublonHelper::isUserProtected($current_user, $protection_type);
+		switch ($protection_type) {
+			case RublonHelper::PROTECTION_TYPE_MOBILE:
+				$result .= '<p>' . sprintf(__('Your account is protected by <a href="%s" target="_blank">Rublon</a>.', 'rublon'), RublonHelper::rubloncomUrl()) . '</p>';
+				break;
+			case RublonHelper::PROTECTION_TYPE_EMAIL:
+				$result .= '<p>' . sprintf(__('Your account is protected by <a href="%s" target="_blank">Rublon</a>.', 'rublon'), RublonHelper::rubloncomUrl())
+					. ' ' . sprintf(__('Get the <a href="%s/get" target="_blank">Rublon mobile app</a> for more security.', 'rublon'), RublonHelper::rubloncomUrl()) . '</p>';
+				break;
+			case RublonHelper::PROTECTION_TYPE_NONE:
+				$lang = RublonHelper::getBlogLanguage();
+				$result .= '<p>' . sprintf(
+					'<span style="color: red; font-weight: bold;">' . __('Warning!', 'rublon') . '</span>'
+						. ' ' . __('Your account is not protected. Go to <a href="%s">your profile page</a> to enable account protection.', 'rublon'),
+					admin_url(RublonHelper::WP_PROFILE_PAGE . RublonHelper::WP_PROFILE_EMAIL2FA_SECTION)
+				) . '</p>';
+				break;
+		}
+
+		$result .= $this->getDeviceWidget();
+		if ($withConsumerScript) {
+			$result .= $this->getConsumerScript();
+		}
+
+		return $result;
+
+	}
+
+
+	/**
+	 * Create Trusted Devices Widget container for WP Dashboard
+	 *
+	 * @return string
+	 */
+	public function getACMWidget($withConsumerScript = true) {
+	
+		$result = '';
+
+		$result .= $this->getShareAccessWidget();
+		if ($withConsumerScript) {
+			$result .= $this->getConsumerScript();
+		}
+	
+		return $result;
+	
+	}
+
+	
+	
+	public function renderConsumerScript() {
+		
+		wp_enqueue_script('jquery');
+		
+		// Consumer script
+		echo $this->getConsumerScript();
+		
+		// Logout listener callback function.
+		// URL is created manually because the wp_logout_url() function escapes the ampersand.
+		$args = array(
+			'action' => 'logout',
+			'redirect_to' => urlencode( RublonHelper::getRublon()->getCurrentUrl() ),
+			'_wpnonce' => wp_create_nonce( 'log-out' )
+		);
+		$logout_url = add_query_arg($args, site_url('wp-login.php', 'login'));
+		echo '<script type="text/javascript">
+		function RublonLogoutCallback() {
+			if (jQuery) {
+				jQuery.post('. json_encode(admin_url('admin-ajax.php')) .', {action: "rublon_logout"}, function(response) {
+					location.reload();
+				});
+			} else {
+				location.reload();
+			}
+		}
+		</script>';
+	}
+
+
+	public function userBox() {
+
+		return '';
+
+	}
+
+
+}
---
+++ uninstall.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Code to be run during plug-in uninstall process
+ *
+ * @package   rublon2factor
+ * @author     Rublon Developers http://www.rublon.com
+ * @copyright  Rublon Developers http://www.rublon.com
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License, version 2  
+ */
+
+if (!current_user_can('activate_plugins')) {
+	exit;
+}
+
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+	exit;
+}
+
+require_once dirname(__FILE__) . '/includes/rublon2factor_helper.php';
+require_once dirname(__FILE__) . '/includes/rublon2factor_multisite_helper.php';
+
+/**
+ * Perform database and options clean-up before deleting plug-in
+ */
+function rublon2factor_plugin_uninstall() {
+
+	global $wpdb;
+
+	// Clear settings for all sites
+	if (RublonMultisiteHelper::isMultisite()) {
+		RublonMultisiteHelper::uninstallMultisite();
+	} else {
+		delete_option(RublonHelper::RUBLON_SETTINGS_KEY);
+		delete_option(RublonHelper::RUBLON_ADDITIONAL_SETTINGS_KEY);
+		delete_option(RublonHelper::RUBLON_OTHER_SETTINGS_KEY);
+		delete_option(RublonHelper::RUBLON_REGISTRATION_SETTINGS_KEY);
+		
+		// Bulk delete user meta
+		$wpdb->query($wpdb->prepare("DELETE FROM $wpdb->usermeta WHERE meta_key IN (%s, %s, %s)",
+			RublonHelper::RUBLON_META_PROFILE_ID,
+			RublonHelper::RUBLON_META_USER_PROTTYPE,
+			RublonHelper::RUBLON_META_AUTH_CHANGED_MSG
+		));
+
+		$wpdb->query($wpdb->prepare("DELETE FROM $wpdb->usermeta WHERE meta_key LIKE %s",
+			RublonHelper::RUBLON_META_DEVICE_ID . '%'
+		));
+
+// 		$all_user_ids = get_users(array(
+// 			'fields' => 'id',
+// 		));
+// 		foreach ($all_user_ids as $user_id) {
+// 			delete_user_meta($user_id, RublonHelper::RUBLON_META_PROFILE_ID);
+// 			delete_user_meta($user_id, RublonHelper::RUBLON_META_USER_PROTTYPE);
+// 			delete_user_meta($user_id, RublonHelper::RUBLON_META_AUTH_CHANGED_MSG);
+// 		}		
+
+	}
+
+}
+
+// Call the clean-up function
+rublon2factor_plugin_uninstall();
